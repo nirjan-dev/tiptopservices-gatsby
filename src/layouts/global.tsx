ab8e747b2244c218aa6e0af13e1c204eaea38bbd
Nirjan Khadka <webdev.nk@gmail.com>
2019-11-10 18:20:44 +0545

    finish mobile nav

--- src/layouts/global.tsx
+++ src/layouts/global.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { ThemeProvider, CSSReset } from '@chakra-ui/core'
 import { customTheme } from '../theme'
+import Nav from './nav'
 
 function GlobalLayout({children}) {
     return (
         <ThemeProvider theme={customTheme}>
             <CSSReset/>
+            <Nav/>
             {children}
         </ThemeProvider>
     )
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,12 @@
 import React from "react"
 import GlobalLayout from "../layouts/global"
-import {Button} from "@chakra-ui/core";
-import {IoIosAlarm } from "react-icons/io";
+
 export default () => {
     return (
         <GlobalLayout>
-            <Button leftIcon={IoIosAlarm} variantColor="primary">This is a button</Button>
+            
         </GlobalLayout>
     )
 }
 
+ 
