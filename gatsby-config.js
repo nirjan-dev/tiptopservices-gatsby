94465bf5f8906ad69c70dbd3cda616f3c4caafbe
Nirjan Khadka <nk13@pop-os.localdomain>
2020-02-07 08:20:27 +0545

    added google analytics and sitemap

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,6 +2,10 @@ require("dotenv").config({
   path: ".env",
 })
 module.exports = {
+  siteMetadata: {
+    // sitemap plugin needs this
+    siteUrl: `https://tiptopservices.com.au`,
+  },
   plugins: [
     `gatsby-plugin-typescript`,
     {
@@ -21,5 +25,15 @@ module.exports = {
         token: process.env.SANITY_TOKEN,
       },
     },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // The property ID; the tracking code won't be generated without it
+        trackingId: "UA-130491215-2",
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+        head: false,
+      },
+    },
+    `gatsby-plugin-sitemap`,
   ],
 }
